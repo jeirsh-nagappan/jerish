441a13f6830e8d1b0bb9a08778ee7f03490478ee
jerishnagappan <jerish.nagappan@hashagile.com>
2024-11-19 21:58:40 +0530

    first .

---
+++ web.js
@@ -0,0 +1,15 @@
+
+         //this is the alert
+       
+        // alert('jeri') 
+
+
+//variables
+        // let score = 10
+        // console.log("score is " + score)
+
+
+//getting user input
+
+        // let name = prompt('what is your name')
+        // console.log(name)
